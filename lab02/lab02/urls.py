da584a76d90b9c2b488675bae84663ccea9e8b34
axcel puma <axcel.puma@tecsup.edu.pe>
2022-03-26 01:31:19 -0500

    lab02

--- lab02/lab02/urls.py
+++ lab02/lab02/urls.py
@@ -1,4 +1,4 @@
-"""lab01 URL Configuration
+"""lab02 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,15 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<h1><center>Hola Mundo de Axcel Puma</h1><center>")
-
+from django.urls import include,path
 
 urlpatterns = [
-    path('',home),
+    path('volumenc/',include('volumenc.urls')),
+    path('calculadora/',include('calculadora.urls')),
+    path('encuesta/',include('encuesta.urls')),
     path('admin/', admin.site.urls),
 ]
